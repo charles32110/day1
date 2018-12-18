1c1eb74b1471a7a2c7fea62c8fcc2e00fbac0eff
charles32110 <596147986@qq.com>
2018-12-18 23:29:13 +1300

    firstday

--- week1.py
+++ week1.py
@@ -1,1 +1,27 @@
+# -*- coding: utf-8 -*-
+#caculator demo&computer memory
+a = 2 ** 5
+b = 9 / 3
+c = 5 / 2
+d = 2 / 3
+e = 4 // 2
+f = 5 // 3
+g = 4 % 2
+h = 5 % 3
+print a,b,c,d,e,f,g,h
+#32 3 2 0 2 1 0 2
 
+
+
+#function   ask for help(max) from the console
+s = max(a,b,c)
+print s
+
+
+def f(x):
+    return x ** 2
+print f(2)
+
+def triangle(base, height):
+    return base * height / 2
+print triangle(2,3)
