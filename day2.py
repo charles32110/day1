46cab7f235a247fe213b00b47e54a06a39b6326e
charles32110 <596147986@qq.com>
2018-12-19 16:11:49 +1300

    second_day

---
+++ day2.py
@@ -0,0 +1,37 @@
+#string
+a = 'hello, world'
+#double
+b = "hello, I'm Charles."
+#changable quates  \
+c = 'hello, I\'m Charles'
+
+#input and output
+print a, b, c
+
+#function help
+#>>help(abs)
+
+#type of valur
+"""
+float >> float 
+num >> num
+int >> int
+int + float >> float
+
+"""
+
+#reuse
+def rectangular(base, height):
+    return (base* height)
+
+def triangle(base, height):
+    return rectangular(base, height)/2
+
+print (triangle(2,3))
+
+
+
+
+
+
+
