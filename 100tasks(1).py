101e944ab3b87db04136c9c9a757fe2c12e5fe13
charles32110 <596147986@qq.com>
2018-12-22 23:07:59 +1300

    100tasks

--- 100tasks(1).py
+++ 100tasks(1).py
@@ -3,6 +3,7 @@
 #the tasks source is from
 # http://www.runoob.com/python/python-exercise-example1.html
 #有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+"""
 diff = []
 same = []
 for a in range(1,5):
@@ -24,7 +25,8 @@ print 'same contain num:' , same
 # 20万到40万之间时，高于20万元的部分，可提成5%；
 # 40万到60万之间时高于40万元的部分，可提成3%；
 # 60万到100万之间时，高于60万元的部分，可提成1.5%，
-# 高于100万元时，超过100万元的部分按1%提成，从键盘输入当月利润I，求应发放奖金总数？
+# 高于100万元时，超过100万元的部分按1%提成，
+# 从键盘输入当月利润I，求应发放奖金总数？
 
 
 def profit(i):
@@ -40,3 +42,41 @@ def profit(i):
 
 q = profit(input("利润："))
 print q
+
+
+
+#一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
+#答案网上有，今天浪一下，不想分析，
+
+for i in range(1,85):
+    if 168 % i == 0:
+        j = 168 / i;
+        if  i > j and (i + j) % 2 == 0 and (i - j) % 2 == 0 :
+            m = (i + j) / 2
+            n = (i - j) / 2
+            x = n * n - 100
+            print(x)
+"""
+#输入某年某月某日，判断这一天是这一年的第几天？
+x = input("year:")
+y = input("month:")
+z = input("day:")
+month = [1,2,3,4,5,6,7,8,9,10,11,12]
+if x % 4 == 0:
+    a = 29
+else:
+    a = 28
+daylist = [31, a, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#注意，liststart from 0
+if y in month and z <= daylist[y-1]:
+    y = y-1
+    while (y > 0):
+        y = y - 1
+        z += daylist[y]
+
+
+    print ("this is %dth day")%z
+else:
+    print ("wrong date")
+
+
