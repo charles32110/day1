cb104e1865548349921d1fdf216fce0e5a36d47f
charles32110 <596147986@qq.com>
2019-03-06 10:57:33 +0800

    basic sorting

--- sorting1.py
+++ sorting1.py
@@ -9,5 +9,51 @@ def bubble_sort(alist):
 				alist[j], alist[j+1] = alist[j+1], alist[j]
 
 seq = [2,4,1,6,34,76,44,55]
-bubble_sort(seq)
+# bubble_sort(seq)
 
+def selected_sort(seq):
+	n = len(seq)
+	for i in range(n -1):
+		min = i
+		for j in range(i+1,n):
+			if seq[j] < seq[min]:
+				min = j
+		if min != i:
+			seq[i], seq[min] = seq[min],seq[i]
+
+
+# selected_sort(seq)
+
+
+def insert_sort(seq):
+	n = len(seq)
+	for i in range(1,n):
+		value = seq[i]
+		pos = i
+		while pos > 0 and value < seq[pos-1]:
+			seq[pos] = seq[pos-1]
+			pos -= 1
+		seq[pos] = value
+		print(seq)
+
+def shell_sort(alist):
+
+	n = len(alist)
+	gap = n // 2
+	while gap >= 1:
+		for j in range(gap, n):
+			i = j
+			while (i - gap) >= 0:
+				if alist[i] < alist[i - gap]:
+					alist[i], alist[i - gap] = alist[i - gap], alist[i]
+					i -= gap
+				else:
+					break
+		gap //= 2
+
+
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+shell_sort(alist)
+print(alist)
